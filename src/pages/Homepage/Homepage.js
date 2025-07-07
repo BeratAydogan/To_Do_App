144e2e702d42febe0e604dbb719b8d0fc931676d
BeratAydogan <berataydogan@ogrenci.beykoz.edu.tr>
2025-07-07 20:26:44 +0300

    Local Storage Added
    With this update, in local storage, you can save data, get data  and delete data.

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,17 +1,60 @@
 import React, { useState, useEffect } from 'react'
-import { View, SafeAreaView, Text, StyleSheet, AsyncStorage, Button,TextInput, FlatList } from 'react-native'
-
-
-
+import { View, SafeAreaView, Text, StyleSheet, Button,TextInput, FlatList, TouchableOpacity } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 export default Homepage = () =>{
 
     const [todoList, setTodoList] = useState([])
     const [query, setQuery] = useState()
 
+    useEffect(() => {
+  
+  fetchData();
+}, []);
+
+const fetchData = async () => {
+    const data = await getData();
+    if (data) setTodoList(data);
+  };
+
+    const storeData = async (value) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem('my-key', jsonValue);
+  } catch (e) {
+    // saving error
+  }
+};
+
+    const getData = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem('my-key');
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    // error reading value
+  }
+};
+
+    const removeData = (index) =>{
+        try {
+            const array = todoList
+            if(index!=-1){
+                array.splice(index,1)
+                setTodoList(array)
+                storeData(array)
+                fetchData()
+            }
+        } catch (error) {
+            
+        }
+    }
+
     const ToDoCard = ({item, index}) => {
         return(
-            <View>
+            <View style={{flexDirection:'row'}}>
                 <Text>{item} - {index}</Text>
+                <TouchableOpacity onPress={()=>removeData(index)}>
+                <Text style={{color:'red', fontSize:30, alignContent:'center',}}>-</Text>
+                </TouchableOpacity>
             </View>
         )
     }
@@ -20,7 +63,9 @@ export default Homepage = () =>{
         <View>
             <Text>HomePage</Text>
             <TextInput placeholder="Enter Todo" onChangeText={(text) => {setQuery(text)}}/>
-            <Button title="+" onPress={() => {setTodoList([...todoList, query])}}/>
+            <Button title="+" onPress={() => {setTodoList([...todoList, query])
+                storeData(todoList)
+            }}/>
             
             <FlatList
             data={todoList}
