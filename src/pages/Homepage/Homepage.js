b7e7aa3e1a5a0db3749c1a897cd96afad592aec0
BeratAydogan <berataydogan@ogrenci.beykoz.edu.tr>
2025-07-07 17:59:01 +0300

    To-Do App created

---
+++ src/pages/Homepage/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, SafeAreaView, Text, StyleSheet } from 'react-native'
+
+export default Homepage = () =>{
+
+    return(
+        <View>
+            <Text>HomePage</Text>
+        </View>
+    )
+}
