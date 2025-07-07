dd1994a61a703e3e9f586285168d2361c2764fa4
BeratAydogan <berataydogan@ogrenci.beykoz.edu.tr>
2025-07-07 18:44:07 +0300

    To-Do's can be added
    in this update, you can create to-do list and see array elements index.

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,11 +1,32 @@
-import React from 'react'
-import { View, SafeAreaView, Text, StyleSheet } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, SafeAreaView, Text, StyleSheet, AsyncStorage, Button,TextInput, FlatList } from 'react-native'
+
+
 
 export default Homepage = () =>{
+
+    const [todoList, setTodoList] = useState([])
+    const [query, setQuery] = useState()
+
+    const ToDoCard = ({item, index}) => {
+        return(
+            <View>
+                <Text>{item} - {index}</Text>
+            </View>
+        )
+    }
 
     return(
         <View>
             <Text>HomePage</Text>
+            <TextInput placeholder="Enter Todo" onChangeText={(text) => {setQuery(text)}}/>
+            <Button title="+" onPress={() => {setTodoList([...todoList, query])}}/>
+            
+            <FlatList
+            data={todoList}
+            renderItem={({item, index}) =><ToDoCard item={item} index={index}/>}
+            keyExtractor={(item,index)=>index.toString()}
+            />
         </View>
     )
 }
